1d78be408d808c615466821edf164b234f8a8dcb
SherryH <enjoy.wind7rain@gmail.com>
2017-01-08 17:52:17 -0800

    Passed all tests

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
   // TODO..
+  constructor(){
+    super();
+    this.age=40;
+    this.job='gamble';
+    this.canFly=false;
+    this.color = 'grey';
+  }
+  forage(){
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(){
+    this.treasureChest.push('treasure');
+  }
 };
